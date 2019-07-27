6c0d1e59c9a9f63e35f2f52803a8c6dd260b601e
Jeyhunj <jeyhunmj@code.edu.az>
2019-07-27 23:09:01 +0400

    task1.htmlandjs-modified

--- task1/js/jslist.js
+++ task1/js/jslist.js
@@ -2,7 +2,7 @@
 
 let students = []
 let inputs = document.getElementsByClassName("student-input");
-let TdItems = "";
+// let TdItems = "";
 
 let stuName = document.querySelector("#name");
 let stuSurname = document.querySelector("#surname");
@@ -22,7 +22,39 @@ function Student(name, surname, email, birthday) {
     this.birthday = birthday;
 };
 
+function checkEmail() {
+    var email = document.getElementById('email');
+    var filter = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!filter.test(email.value)) {
+        alert('Please provide a valid email address');
+        email.focus;
+        return false;
+    }
+}
+
+function emptyalert() {
+    if(document.getElementById("name").value == 0,document.getElementById("surname").value == 0,document.getElementById("email").value == 0,document.getElementById("birthday").value == 0)
+   {
+       alert("Please be sure  you fill them all")
+   }  else{
+       addStudent()
+       
+   } 
+   }
+
 function addStudent() {
     let student = new Student(stuName.value, stuSurname.value, stuEmail.value, stuBirthday.value);
     
+    students.push(student);
+
+    let tr = document.createElement("tr");
+    document.querySelector(".student-list").appendChild(tr);
+    tr.innerHTML = `<td>${stuName.value}</td>
+                    <td>${stuSurname.value}</td>
+                    <td>${stuEmail.value}</td>
+                    <td>${stuBirthday.value}</td>`
+                   
 }
+
+
+
